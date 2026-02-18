47df034f10baae6cb385621a4ccc913241b545c4
AzNaNa <supersochara@gmail.com>
2026-02-18 20:58:47 +0300

    adding new files and photos

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -17,12 +17,13 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: 'PASTE_YOUR_API_KEY',
-  authDomain: 'PASTE_YOUR_AUTH_DOMAIN',
-  projectId: 'PASTE_YOUR_PROJECT_ID',
-  storageBucket: 'PASTE_YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'PASTE_YOUR_MESSAGING_SENDER_ID',
-  appId: 'PASTE_YOUR_APP_ID'
+  apiKey: "AIzaSyDN6uLWDyZWiy6hZul4tYH8jUWO6Hb1yAI",
+  authDomain: "our-future-ef56a.firebaseapp.com",
+  projectId: "our-future-ef56a",
+  storageBucket: "our-future-ef56a.firebasestorage.app",
+  messagingSenderId: "99356391127",
+  appId: "1:99356391127:web:6fe72f2421ad63c5716292",
+  measurementId: "G-VQQRDH9186"
 };
 
 let app;
